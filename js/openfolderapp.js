db6241db11259ed11f3a8596399e7ef030e0bee2
Chanel Mepschen <chanelmepschen856@hotmail.com>
2017-11-16 14:43:49 +0100

    Changed the addEventListener and added check for 1 click or 2 clicks

--- js/openfolderapp.js
+++ js/openfolderapp.js
@@ -1,12 +1,20 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 (function(){
-    var folders = document.querySelectorAll('.top-nav__item'),
-        i;
+    var folderContainer = document.querySelector('.top-nav'),
+        folders = document.querySelectorAll('.top-nav__item');
+    var clickCount = 0;
+    var singleClickTimer;
 
     var item = {
         //Function that will handle the single click
-        select: function(e){
-            console.log('single click worked: ' + e.target);
+        select: function(parent){
+            console.log(parent);
+            if(parent.getAttribute('class') == 'top-nav__item'){
+                for(var i = 0; i < folders.length; i++){
+                    folders[i].classList.remove('top-nav__item_selected');
+                }
+                parent.classList.add('top-nav__item_selected');
+            }
         },
         //Function that will handle the double click
         open: function(e){
@@ -14,22 +22,21 @@
         }
     };
 
-    //Add one click for selection and double click for opening folder/file/application
-    for (i = 0; i < folders.length; i++){
-        var clickCount = 0;
-        folders[i].addEventListener('click', function(){
-            clickCount++;
-            if(clickCount === 1){
-                 var singleClickTimer = setTimeout(function(){
-                     clickCount = 0;
-                     item.open();
-                 }, 400);
-            } else if (clickCount ===2){
-                clearTimeout(singleClickTimer);
+    function checkAmountOfClicks(e){
+        console.log(e);
+        clickCount++;
+        if(clickCount === 1){
+            singleClickTimer = setTimeout(function(){
                 clickCount = 0;
-                item.select();
-            }
-        });
+                item.select(e.target.parentElement.parentElement);
+            }, 200);
+        } else if(clickCount === 2){
+            clearTimeout(singleClickTimer);
+            clickCount = 0;
+            item.open(e);
+        }
     }
+
+    folderContainer.addEventListener('click', checkAmountOfClicks);
 })();
 //Single and double click function by Karbassi: https://gist.github.com/karbassi/639453
