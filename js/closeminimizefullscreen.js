5ede4fe24e34ff9e0df007f6a333cca207307491
Chanel Mepschen <chanelmepschen856@hotmail.com>
2017-11-17 11:27:50 +0100

    Close button now works

--- js/closeminimizefullscreen.js
+++ js/closeminimizefullscreen.js
@@ -1,20 +1,35 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 (function(){
-    var buttonsWrap = document.querySelectorAll('.buttons-wrap');
+    var buttonsWrap = document.querySelectorAll('.buttons-wrap'),
+        i;
 
-    for(var i = 0; i < buttonsWrap.length; i++){
+    //Close window with the id of the section
+    function closeWindow(id){
+        document.querySelector('#' + id).classList.add('desktop-folder_hidden');
+    }
+
+    //Trigger the right function for the right button.
+    function forwardToFunction(el){
+        var windowSection = el.parentNode.parentNode.getAttribute('id');
+
+        if (el.className.includes('close')){
+            closeWindow(windowSection);
+        }
+
+        if (el.className.includes('minimize')){
+            // Function reference for minimize here
+            console.log('minimize');
+        }
+
+        if (el.className.includes('fullscreen')){
+            // Function reference for fullscreen here
+            console.log('fullscreen');
+        }
+    }
+
+    for(i = 0; i < buttonsWrap.length; i++){
         buttonsWrap[i].addEventListener('click', function(e){
-            console.log(e.target.parentNode);
-            if (e.target.parentNode.className.includes('close')){
-                // Function reference for close here
-                console.log('close');
-            } else if (e.target.parentNode.className.includes('minimize')){
-                // Function reference for minimize here
-                console.log('minimize');
-            } else if (e.target.parentNode.className.includes('fullscreen')){
-                // Function reference for fullscreen here
-                console.log('fullscreen');
-            }
+            forwardToFunction(e.target);
         });
     }
 })();
