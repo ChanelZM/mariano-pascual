5cb2141778cd63cb0d3919134c0cb34d91a420e2
Chanel Mepschen <chanelmepschen856@hotmail.com>
2018-01-02 18:46:24 +0100

    Added screen area to settings

--- js/settings.js
+++ js/settings.js
@@ -5,9 +5,20 @@
         changeBodyColor(e.target.id);
     }
 
+    function getValue(){
+        val = document.querySelector('.range').value;
+        console.log(val);
+        if (val == 100){
+            document.querySelector('.body-wrap').style.transform = 'scaleX(' + 1 + ')';
+        } else {
+            document.querySelector('.body-wrap').style.transform = 'scaleX(0.' + val + ')';
+        }
+    }
+
     function changeBodyColor(color){
-        document.querySelector('body').className = 'body-overflow-h' + ' ' + color;
+        document.querySelector('.body-wrap').className = 'body-wrap' + ' ' + color;
     }
 
     document.querySelector('.color-list').addEventListener('click', getClickedColor);
+    document.querySelector('.range').addEventListener('change', getValue);
 })();
