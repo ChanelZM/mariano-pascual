b435a451db2b2699f7037131a1962a6fca0a69cd
Chanel Mepschen <chanelmepschen856@hotmail.com>
2018-03-05 16:47:09 +0100

    Changed window style

--- js/bundle.js
+++ js/bundle.js
@@ -57,32 +57,6 @@
             closeWindow(id, 'hidden');
         });
     }
-
-    // //Trigger the right function for the right button.
-    // function forwardToFunction(el, targetWindow, classNam){
-    //     console.log(targetWindow);
-    //     var id = targetWindow.getAttribute('id');
-    //
-    //     if (el.className.includes('close')){
-    //         closeWindow(id, classNam);
-    //     }
-    //
-    //     if (el.className.includes('minimize')){
-    //         // Function reference for minimize here
-    //         console.log('minimize');
-    //     }
-    //
-    //     if (el.className.includes('fullscreen')){
-    //         // Function reference for fullscreen here
-    //         console.log('fullscreen');
-    //     }
-    // }
-
-    // for(i = 0; i < buttonsWrap.length; i++){
-    //     buttonsWrap[i].addEventListener('click', function(e){
-    //         forwardToFunction(e.target, e.target.parentNode.parentNode, 'hidden');
-    //     });
-    // }
 })();
 
 },{}],3:[function(require,module,exports){
